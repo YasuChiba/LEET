0d368feef4ca7e632f7cb3273ee8352677b09fab
LAPTOP-JSQPQCOB\Jhern <jhernawa@ucsd.edu>
2017-11-15 16:35:20 -0800

    new files added for meals

---
+++ app/src/main/java/com/leet/leet/screen/meal_base/meal_base_view/MealBaseView.java
@@ -0,0 +1,10 @@
+package com.leet.leet.screen.meal_base.meal_base_view;
+
+/**
+ * Created by Jhern on 11/15/2017.
+ */
+
+public class MealBaseView {
+    
+
+}
--- app/src/main/java/com/leet/leet/screen/meal_base/screen/meal_main/view/MealMainFragment.java
+++
@@ -1,8 +0,0 @@
-package com.leet.leet.screen.meal_base.screen.meal_main.view;
-
-/**
- * Created by Jhern on 11/15/2017.
- */
-
-public class MealMainFragment {
-}
