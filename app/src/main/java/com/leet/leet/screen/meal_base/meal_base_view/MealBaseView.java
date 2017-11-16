bce4ef38af474bdb830dec3d87fa0ae1689b3739
LAPTOP-JSQPQCOB\Jhern <jhernawa@ucsd.edu>
2017-11-15 16:49:32 -0800

    trying to connect two views

--- app/src/main/java/com/leet/leet/screen/meal_base/meal_base_view/MealBaseView.java
+++ app/src/main/java/com/leet/leet/screen/meal_base/meal_base_view/MealBaseView.java
@@ -1,10 +1,24 @@
 package com.leet.leet.screen.meal_base.meal_base_view;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.leet.leet.R;
+
 /**
  * Created by Jhern on 11/15/2017.
  */
 
 public class MealBaseView {
-    
+
+        private View mRootView;
+
+        public MealBaseView(LayoutInflater inflater, ViewGroup container) {
+            mRootView = inflater.inflate(R.layout.view_meal, container, false);
+
+        }
+        public
+    }
 
 }
